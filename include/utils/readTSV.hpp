322ee7217bb1e964b5aad0d9fa1d2e1217fec20d
Luca Olivieri <luca.olivieri-1@unitn.it>
2020-01-28 17:06:55 +0100

    Added TSV file loader

---
+++ include/utils/readTSV.hpp
@@ -0,0 +1,38 @@
+#ifndef READTSV_H
+#define READTSV_H
+
+#include <Eigen/Core>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+using namespace Eigen;
+
+vector<MatrixXd> readTSV(const char filename[], int rows, int cols, char delimiter='\t')
+{
+    fstream fin(filename, ios::in); // File pointer
+    vector<MatrixXd> result;
+    string line;
+    string splitted;
+    MatrixXd* res;
+
+    while (getline(fin, line)) {
+        
+        stringstream s(line); // used for breaking words
+        int i = 0;
+        res = new MatrixXd(rows, cols); // new matrix to contain data
+
+        while (getline(s, splitted, delimiter)) {
+            res->operator()(i % rows, i / rows) = stod(splitted);; // So ugly I can't even
+            i++;
+        }
+
+        result.push_back(*res);
+    }
+
+    return result;
+}
+
+#endif
