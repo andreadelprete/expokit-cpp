e19a4e7f7418d5de512f86d9222c117a96bd41dc
Luca Olivieri <luca.olivieri-1@unitn.it>
2020-03-14 12:27:13 +0100

    First results with balacing

--- run/runBalancing.cpp
+++ run/runBalancing.cpp
@@ -5,10 +5,10 @@
 
 #include "LDS2OrderUtility.hpp"
 
+#include "utils/errorComputing.hpp"
 #include "utils/readTSV.hpp"
 #include "utils/statistics.hpp"
 #include "utils/stop-watch.h"
-#include "utils/errorComputing.hpp"
 
 using namespace std;
 using namespace expokit;
@@ -17,7 +17,6 @@ using namespace Eigen;
 #define N 4
 #define M N * 3 * 2
 #define TIMESTEP 0.005
-
 
 int main()
 {
@@ -33,13 +32,40 @@ int main()
     Matrix<double, M, 1> ref;
     Vector2d e;
 
-    
-    Matrix4d A;
-    A << 0, 0, 1, 0, 0, 0, 0, 1, 1000000, 0, 1000, 0, 0, 1000000, 0, 1000;
+    // DeltaOFF_TSOFF_TVOFF
+    LDSUtility<double, M> d0t0v0;
 
-    MatrixExponential<double, Dynamic> expp(4);
+    // DeltaX_TSOFF_TVON
+    LDSUtility<double, M> dxt0v1;
+    dxt0v1.useTV(true);
+    dxt0v1.setTVSquarings(5);
 
-    expp.balancing2(A);
+    for (unsigned int i = 0; i < vecA.size(); ++i) {
+        START_PROFILER("ComputeIntegralXt");
+        d0t0v0.ComputeIntegralXt(vecA[i], vecb[i], vecxInit[i], TIMESTEP, ref);
+        STOP_PROFILER("ComputeIntegralXt");
+        // cout << ref << endl;
+        stats.store("ComputeIntegralXt", d0t0v0.getSquarings());
 
-    cout << A << endl;
+        START_PROFILER("BalancedComputeIntegralXt");
+        d0t0v0.BalancedComputeIntegralXt(vecA[i], vecb[i], vecxInit[i], TIMESTEP, out);
+        STOP_PROFILER("BalancedComputeIntegralXt");
+        // cout << out << endl;
+        stats.store("BalancedComputeIntegralXt", d0t0v0.getSquarings());
+        e = computeErrorIntegral24(out, ref, TIMESTEP * i);
+        stats.store("ERROR2___BalancedComputeIntegralXt", e(0));
+        stats.store("ERRORINF_BalancedComputeIntegralXt", e(1));
+
+        START_PROFILER("BalancedComputeIntegralXtTV");
+        dxt0v1.BalancedComputeIntegralXt(vecA[i], vecb[i], vecxInit[i], TIMESTEP, out);
+        STOP_PROFILER("BalancedComputeIntegralXtTV");
+        // cout << out << endl;
+        stats.store("BalancedComputeIntegralXtTV", dxt0v1.getSquarings());
+        e = computeErrorIntegral24(out, ref, TIMESTEP * i);
+        stats.store("ERROR2___BalancedComputeIntegralXtTV", e(0));
+        stats.store("ERRORINF_BalancedComputeIntegralXtTV", e(1));
+    }
+
+    getProfiler().report_all(3);
+    stats.report_all();
 }
