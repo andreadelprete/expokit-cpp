df044e7557f1abb948e25121519adc49cdf72d48
Andrea Del Prete <andre.delprete@gmail.com>
2020-03-22 23:46:43 +0100

    Implement new Balancing (Parlett and Reinsch) algorithm (which should be the one of LAPACK)

--- scripts/test_matrix_balancing_vs_time_scaling.py
+++ scripts/test_matrix_balancing_vs_time_scaling.py
@@ -41,6 +41,68 @@ def expm(A, use_exact_onenorm="auto", verbose=False):
         X = X.dot(X)
     return X
 
+def balance_parlett(A, max_iter=None):
+    n = A.shape[0]
+    assert(A.shape[1]==n)
+    B = np.copy(A)  # balanced matrix
+    D = np.ones(n)  # diagonal elements of similarity transformation    
+    converged = False
+    if(max_iter is None):
+        max_iter = n*n
+        
+    for it in range(max_iter):        
+        converged = True
+        for i in range(n):
+            c = norm(B[:,i], 1)
+            r = norm(B[i,:], 1)
+            s = c+r
+            f = 1
+
+            while c < 0.5*r:
+                c *= 2.0
+                r *= 0.5
+                f *= 2.0
+                if(f>2**20):
+                    break
+            while c > 2.0*r:
+                c *= 0.5
+                r *= 2.0
+                f *= 0.5
+                if(f<2**(-20)):
+                    break
+            
+            if(c+r < 0.95*s):
+                if(D[i]*f<2**(-20)):
+                    f = 2**(-20) / D[i]
+                    if(f!=1.0):
+                        converged = False
+                elif(D[i]*f>2**(20)):
+                    f = 2**(20) / D[i]
+                    if(f!=1.0):
+                        converged = False
+                else:
+                    converged = False
+                D[i] *= f
+                B[:,i] *= f
+                B[i,:] /= f
+        
+        if(converged):
+            print("Balancing converged in %d iterations"%it)    
+            break
+    
+    if(not converged):
+        print("ERROR: balancing algorithm did not converge!")
+        
+    # check that B = Dinv * A * D
+#    Dm = np.diagflat(D)
+#    Dinv = np.linalg.inv(Dm)
+#    B2 = Dinv * A * Dm
+#    print('A\n', A)
+    print('log2(D)\n', np.log2(D).T)
+#    print('B\n', B)
+#    print('Dinv*A*D\n', B2)
+    return B, np.diagflat(D)
+    
 def new_balance(A, max_iter=None):
     n = A.shape[0]
     assert(A.shape[1]==n)
@@ -73,8 +135,7 @@ def new_balance(A, max_iter=None):
         iMin = 0
         for k in range(n):
             if(k==jMax):
-                for kk in range(n):
-                    cNew[jMax, kk] = c[kk]+abs(B[jMax,kk])
+                cNew[jMax, :] = c+np.abs(B[jMax,:])
                 cNew[jMax, jMax] = 0.5*c[jMax]
                 v[jMax] = np.max(cNew[jMax,:])
             else:
@@ -92,6 +153,7 @@ def new_balance(A, max_iter=None):
         
         # check convergence
         if(vMin >= c[jMax]):
+            print("Balancing converged in %d iterations"%it)
             converged = True
             break
         
@@ -117,7 +179,7 @@ def new_balance(A, max_iter=None):
 #    Dinv = np.linalg.inv(Dm)
 #    B2 = Dinv * A * Dm
 #    print('A\n', A)
-#    print('log2(D)\n', np.log2(D).T)
+    print('log2(D)\n', np.log2(D).T)
 #    print('B\n', B)
 #    print('Dinv*A*D\n', B2)
     return B, np.diagflat(D)
@@ -136,7 +198,8 @@ def compute_x_T(A, a, x0, T, balance=None):
     z0[-1, 0] = 1.0
     if balance:
         if(balance=='lapack'):
-            C_bal, D = matrix_balance(T*C, permute=False)
+#            C_bal, D = matrix_balance(T*C, permute=False)
+            C_bal, D = balance_parlett(T*C)
         else:
             (C_bal, D) = new_balance(T*C)
         D = np.asmatrix(D)
