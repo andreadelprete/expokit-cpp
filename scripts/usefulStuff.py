58e4cb2d2b89f0d3f1168710ab4bdceb14710b38
Luca Olivieri <luca.olivieri-1@unitn.it>
2020-04-22 15:44:03 +0200

    Include in test also Toolbox matrices

--- scripts/usefulStuff.py
+++ scripts/usefulStuff.py
@@ -20,7 +20,15 @@ def printStats(M, text):
     print('%20s   %10d   %10.2f' % (text, s, np.log2(l1norm)))
 
 
-def test_matrix(A, gt):
+def test_matrix(A):
+    B_scipy, D1 = matrix_balance(A, permute=False)
+    # B_new, D2, Dinv, it = slow_balance2(A)
+    B_new, D2, Dinv, it = new_balance(A)
+
+    return computeGains(norm(B_scipy, 1), norm(B_new, 1))
+
+
+def test_matrix_GT(A, gt):
     B_scipy, D1 = matrix_balance(A, permute=False)
     # B_new, D2, Dinv, it = slow_balance2(A)
     B_new, D2, Dinv, it = new_balance(A)
