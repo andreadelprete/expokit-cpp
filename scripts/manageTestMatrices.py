137ddbf4985b8b60bec7c57fa39f407a98c69548
Luca Olivieri <luca.olivieri-1@unitn.it>
2020-04-22 15:44:03 +0200

    Include in test also Toolbox matrices

--- scripts/manageTestMatrices.py
+++ scripts/manageTestMatrices.py
@@ -8,18 +8,21 @@ from numpy.linalg import norm
 # Matrices are returned into an array and are accessed as such:
 # array[k]['A']
 # also the name of the matrix is present as array[k]['name']
+
+# Disregard previous comment, it return a simple list of matrices
 def importMatrices(path):
     matrices = []
     for filename in os.listdir(os.getcwd() + '/' + path):
-        m = scipy.io.loadmat(path + '/' + filename)
-        m['name'] = os.path.splitext(filename)[0]  # Strip extension
+        m = scipy.io.loadmat(path + '/' + filename)['A'].real.astype('float64')
+        # There is the possibility to trace the MatLab name
+        # m['name'] = os.path.splitext(filename)[0]  # Strip extension
         matrices.append(m)
 
     return matrices
 
 
-# a = importMatrices('scripts/testMatrices')
-# print(a[0]['A'])
+a = importMatrices('scripts/testMatrices')
+print(a[0])
 
 
 def james2014Generator(N):
@@ -35,5 +38,5 @@ def james2014Generator(N):
     return norm(A, 1), np.diagflat(Dinv) @ A @ np.diagflat(D)
 
 
-S = james2014Generator(8)
-print(S)
+# S = james2014Generator(8)
+# print(S)
--- scripts/randomStiff.py
+++ scripts/randomStiff.py
@@ -3,7 +3,7 @@ from numpy.linalg import norm
 import matplotlib.pyplot as plt
 from testingStuff import testStuff
 from usefulStuff import test_matrix, maxnorm  # , generateStiffMatrix
-from manageTestMatrices import james2014Generator  # , importMatrices
+from manageTestMatrices import james2014Generator, importMatrices
 import multiprocessing
 from functools import partial
 
@@ -33,15 +33,29 @@ def run_random_tests(matrix_size, n_tests):
 
         gamma[k], squarings_gain[k], groundTs[k] = test_matrix(A, gt)  # Be aware of what method is called here
 
-        # if gamma[k] < 0:
-        #     test_matrix(A)
-        #     pass
+    printData(gamma, squarings_gain, groundTs)
 
+
+def analyseTheseMatrices(matrices):
+    howMany = len(matrices)
+    gamma = np.empty(howMany)
+    squarings_gain = np.empty(howMany)
+
+    for k in range(0, howMany):
+        gamma[k], squarings_gain[k] = test_matrix(matrices[k])
+
+    printData(gamma, squarings_gain, None, howMany, matrices[0].shape[0])
+
+
+def printData(gamma, squarings_gain, groundTs, n_tests, N):
     print('Matrix size = ', N)
     print('Gamma min-avg-max = %7.3f  %7.3f  %7.3f' % (np.min(gamma), np.mean(gamma), np.max(gamma)))
-    print('groundTs min-avg-max = %7.3f  %7.3f  %7.3f' % (np.min(groundTs), np.mean(groundTs), np.max(groundTs)))
-    print('Percentage of negative gamma = ', 1e2*np.count_nonzero(gamma < 0)/n_tests)
-    print('Percentage of negative groundTs = ', 1e2*np.count_nonzero(groundTs < 0)/n_tests)
+    
+    if groundTs is not None:
+        print('groundTs min-avg-max = %7.3f  %7.3f  %7.3f' % (np.min(groundTs), np.mean(groundTs), np.max(groundTs)))
+        print('Percentage of negative gamma = ', 1e2*np.count_nonzero(gamma < 0)/n_tests)
+        print('Percentage of negative groundTs = ', 1e2*np.count_nonzero(groundTs < 0)/n_tests)
+    
     print('Squarings gain min-avg-max = %7.3f  %7.3f  %7.3f' % (np.min(squarings_gain),
                                                                 np.mean(squarings_gain),
                                                                 np.max(squarings_gain)))
@@ -56,12 +70,13 @@ def run_random_tests(matrix_size, n_tests):
     plt.title('Relative gain in L1 norm for size %d' % N)
     plt.grid(True)
 
-    plt.figure()
-    n, bins, patches = plt.hist(groundTs, 30, facecolor='g', alpha=0.75)
-    plt.xlabel('Ground Truth')
-    plt.ylabel('Frequency')
-    plt.title('Difference from original norm %d' % N)
-    plt.grid(True)
+    if (groundTs is not None):
+        plt.figure()
+        n, bins, patches = plt.hist(groundTs, 30, facecolor='g', alpha=0.75)
+        plt.xlabel('Ground Truth')
+        plt.ylabel('Frequency')
+        plt.title('Difference from original norm %d' % N)
+        plt.grid(True)
 
     plt.figure()
     n, bins, patches = plt.hist(squarings_gain, 20, facecolor='g', alpha=0.75)
@@ -73,14 +88,23 @@ def run_random_tests(matrix_size, n_tests):
     plt.show()
 
 
-MATRIX_SIZES = [4, 8, 16, 32, 64, 128, 256]  # , 512, 1024]  # matrix size
-N_TESTS = 10  # number of tests
+whatToDo = False
 
-np.random.seed(19680801)  # Fixing random state for reproducibility
+if whatToDo:
+    # Section with tests on generated matrices
+    MATRIX_SIZES = [4, 8, 16, 32, 64, 128, 256]  # , 512, 1024]  # matrix size
+    N_TESTS = 10  # number of tests
 
-# for n in MATRIX_SIZES:
-#     run_random_tests(n, N_TESTS)
-# plt.show()
+    np.random.seed(19680801)  # Fixing random state for reproducibility
 
-p = multiprocessing.Pool()
-p.map(partial(run_random_tests, n_tests=N_TESTS), MATRIX_SIZES)
+    p = multiprocessing.Pool()
+    p.map(partial(run_random_tests, n_tests=N_TESTS), MATRIX_SIZES)
+
+    # for n in MATRIX_SIZES:
+    #     run_random_tests(n, N_TESTS)
+    # plt.show()
+else:
+    # Section with tests on Toolbox matrices
+    matrices = importMatrices('scripts/testMatrices')
+
+    analyseTheseMatrices(matrices)
