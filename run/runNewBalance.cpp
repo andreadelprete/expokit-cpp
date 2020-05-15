12e3bd185311aabb93c77ebfe21250ed41852798
Luca Olivieri <luca.olivieri-1@unitn.it>
2020-05-15 12:01:45 +0200

    More dubug

--- run/runNewBalance.cpp
+++ run/runNewBalance.cpp
@@ -14,20 +14,29 @@ using namespace std;
 using namespace expokit;
 using namespace Eigen;
 
-#define N 3
-#define M N * 3 * 2
+#define N 4
 
 int main()
 {
     cout << "Running speed ups benchmark" << endl;
 
+    IOFormat CleanFmt(FullPrecision, 0, ", ", "\n", "[", "]");
+
     MatrixXd A(N, N);
-    A << 1, 2, 300, 4, 5, 6, 0, 0, 0;
+    // A << 1, 2, 300, 4, 5, 6, 0, 0, 0;
+    A <<-3.80466072e-01,  1.19867645e+06,  8.63170116e-02,
+        -2.60920215e+05,
+        1.68822169e+00,  1.56173278e+05,  4.61874965e+05,
+        -9.96831380e+05,
+        1.20856994e+00, -8.61700547e-01, -6.34023305e-01,
+        -9.35935672e-01,
+       -2.87160379e+05,  4.70987189e+05, -7.37804710e-01,
+         6.75828111e-01;
     MatrixXd B(N, N);
     MatrixXd D(N, N);
     MatrixXd Dinv(N, N);
 
-    MatrixExponential<double, Dynamic> util(3);
+    MatrixExponential<double, Dynamic> util(N);
 
     int it = util.newBalancing(A, B, D, Dinv);
 
@@ -36,6 +45,8 @@ int main()
               << "it:" << it << std::endl
               << "B:" << std::endl
               << B << std::endl
+              << "Norm of B:" << std::endl
+              << B.lpNorm<1>() << std::endl
               << "D:" << std::endl
               << D << std::endl
               << "Dinv:" << std::endl
